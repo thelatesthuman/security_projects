98a7960cb065243e44b47771ebe36372cd84d07f
thelatesthuman <jporterqoop@gmail.com>
2025-01-07 20:48:18 -0600

    adding rustpass to security_projects

---
+++ rustpass/target/debug/deps/rustpass-a10bacc96100ac9c.d
@@ -0,0 +1,5 @@
+/home/jport/Github/security_projects/rustpass/target/debug/deps/rustpass-a10bacc96100ac9c: src/main.rs
+
+/home/jport/Github/security_projects/rustpass/target/debug/deps/rustpass-a10bacc96100ac9c.d: src/main.rs
+
+src/main.rs:
---
+++ rustpass/target/debug/rustpass.d
@@ -0,0 +1,1 @@
+/home/jport/Github/security_projects/rustpass/target/debug/rustpass: /home/jport/Github/security_projects/rustpass/src/main.rs
